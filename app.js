d3dd4a6f84243c647faa299e76cf57ebcc488d8f
DonaldoAr <jefe.ceo@gmail.com>
2021-08-31 17:46:04 -0500

    Create a server only using a Node

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const http = require('http');
 
 // Here create a server
 http.createServer((req,res)=>{
-    res.write('Hola mundo');
+    console.log(req);
+    //res.write('Hola mundo');
     // For say node that it finish to write this response
+    //res.writeHead(200,{ 'Content-Type': 'application/json'} );
+    res.setHeader('content-Disposition', 'attatchment; filename=lista.csv');
+    res.writeHead(200,{ 'Content-Type': 'application/csv'} );
+    res.write( 'id, nombre\n' );
+    res.write( '1, Luis\n' );
+    res.write( '2, Donaldo\n' );
+    /*
+    const persona = {
+        id: 1,
+        nombre: 'Luis'
+    }*/
+    // res.write( JSON.stringify( persona ) );
     res.end();
 })
 .listen(8080);
