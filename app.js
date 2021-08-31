912a417548c1ffa4727a38fcf4faaffb0f3730f9
DonaldoAr <jefe.ceo@gmail.com>
2021-08-31 18:34:13 -0500

    Add a Express

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-// It's package from note
-const http = require('http');
-
-// Here create a server
-http.createServer((req,res)=>{
-    console.log(req);
-    //res.write('Hola mundo');
-    // For say node that it finish to write this response
-    //res.writeHead(200,{ 'Content-Type': 'application/json'} );
-    res.setHeader('content-Disposition', 'attatchment; filename=lista.csv');
-    res.writeHead(200,{ 'Content-Type': 'application/csv'} );
-    res.write( 'id, nombre\n' );
-    res.write( '1, Luis\n' );
-    res.write( '2, Donaldo\n' );
-    /*
-    const persona = {
-        id: 1,
-        nombre: 'Luis'
-    }*/
-    // res.write( JSON.stringify( persona ) );
-    res.end();
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res)=> {
+  res.send('Hello World');
 })
-.listen(8080);
 
-console.log('Escuchando en el puerto', 8080);
+app.get('/hola-mundo', (req, res)=> {
+    res.send('EL mundo te saludo :D');
+  })
+ 
+app.get('*', (req, res)=> {
+    res.send('Not found 404');
+ })
+
+app.listen(8080)
