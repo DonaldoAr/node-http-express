688a1581fedbe42364700bd311988ec4ca08bd33
DonaldoAr <jefe.ceo@gmail.com>
2021-09-02 18:15:44 -0500

    Add a html's for static page

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
  
+const port = 8080;
+
+// Servir contenido estatico: middleware
+app.use( express.static('public'))
+
+/* This line doesn't execute because content statis it's execute
 app.get('/', (req, res)=> {
   res.send('Hello World');
 })
-
+*/
 app.get('/hola-mundo', (req, res)=> {
     res.send('EL mundo te saludo :D');
   })
  
 app.get('*', (req, res)=> {
-    res.send('Not found 404');
+    res.sendFile(__dirname + '/public/hola-mundo/404.html');
  })
 
-app.listen(8080)
+app.listen(port, ()=>{
+  console.log(`Example app listen at http://localhost:${port}`);
+})
