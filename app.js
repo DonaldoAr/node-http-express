dc7baf0e4cc59c190bb7584e23b6aa6e09962061
DonaldoAr <jefe.ceo@gmail.com>
2021-09-03 11:59:11 -0500

    Add a new pages

--- app.js
+++ app.js
@@ -11,10 +11,23 @@ app.get('/', (req, res)=> {
   res.send('Hello World');
 })
 */
+/*
 app.get('/hola-mundo', (req, res)=> {
     res.send('EL mundo te saludo :D');
   })
- 
+ */
+  app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/public/index.html');
+ })
+
+ app.get('/generic', (req, res)=> {
+  res.sendFile(__dirname + '/public/generic.html');
+})
+
+app.get('/elements', (req, res)=> {
+  res.sendFile(__dirname + '/public/elements.html');
+})
+
 app.get('*', (req, res)=> {
     res.sendFile(__dirname + '/public/hola-mundo/404.html');
  })
