7eaf992e464ccaaf1805e3be60b8432dc55ac501
DonaldoAr <jefe.ceo@gmail.com>
2021-09-14 03:08:24 -0500

    Add a elements for render

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
-//TOD0: require('hbs');
-app.set('view engine', 'hbs');
+require('dotenv').config();
+const port = process.env.PORT;
 
+// Handlebars for partial register
+var hbs = require("hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+//TOD0: require('hbs');
+app.set("view engine", "hbs");
 
 // Servir contenido estatico: middleware
-app.use( express.static('public'))
+app.use(express.static("public"));
 
 /* This line doesn't execute because content statis it's execute
 app.get('/', (req, res)=> {
@@ -18,14 +23,28 @@ app.get('/hola-mundo', (req, res)=> {
     res.send('EL mundo te saludo :D');
   })
  */
-  app.get('/', (req, res)=> {
-    res.render('home',{
-      nombre: 'Luis Donaldo Arias Garcia',
-      titulo: 'Enviado desde el controlador'
-    });
- })
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Luis Donaldo Arias Garcia",
+    titulo: "Enviado desde el controlador",
+  });
+});
 
- app.get('/generic', (req, res)=> {
+app.get('/elements', (req, res)=> {
+  res.render(__dirname + '/views/elements.hbs',{
+
+  });
+})
+
+app.get('/generic', (req, res)=> {
+  res.render(__dirname + '/views/generic.hbs',{
+    
+  });
+})
+
+
+/*
+app.get('/generic', (req, res)=> {
   res.sendFile(__dirname + '/public/generic.html');
 })
 
@@ -36,7 +55,7 @@ app.get('/elements', (req, res)=> {
 app.get('*', (req, res)=> {
     res.sendFile(__dirname + '/public/hola-mundo/404.html');
  })
-
-app.listen(port, ()=>{
+*/
+app.listen(port, () => {
   console.log(`Example app listen at http://localhost:${port}`);
-})
+});
