224623a72742d18d34a0d86c90e84e46807cce33
DonaldoAr <jefe.ceo@gmail.com>
2021-09-19 09:47:38 -0500

    Ready to use in one server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT;
 
 // Handlebars for partial register
